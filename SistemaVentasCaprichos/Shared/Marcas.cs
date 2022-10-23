b813663e131cfcc3c68d571f104d63143241bb3d
oswald balladares <oswaldballadares37@gmail.com>
2022-10-22 22:36:46 -0600

    Modelo Marcas

---
+++ SistemaVentasCaprichos/Shared/Marcas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SistemaVentasCaprichos.Shared
+{
+    public class Marcas
+    {
+        #region ATRIBUTOS/PROPIEDADES
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30, ErrorMessage = "Máximo 30 dígitos"), MinLength(2, ErrorMessage = "Minimo 2 dígitos")]
+        public string Nombre { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "Máximo 50 dígitos"), MinLength(2, ErrorMessage = "Minimo 2 dígitos")]
+        public string Descripción { get; set; }
+
+        [Required]
+        public DateTime Fecha { get; set; }
+
+        public Boolean Estado { get; set; }
+
+        #endregion
+    }
+}
