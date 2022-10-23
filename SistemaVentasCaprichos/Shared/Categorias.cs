64a6cc0479ba8ce998ce9c50d787ff7fdc989122
oswald balladares <oswaldballadares37@gmail.com>
2022-10-22 22:10:29 -0600

    agregamos el modelo de Categoria

---
+++ SistemaVentasCaprichos/Shared/Categorias.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SistemaVentasCaprichos.Shared
+{
+    public class Categorias
+    {
+        #region ATRIBUTOS/PROPIEDADES
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30, ErrorMessage = "Máximo 30 dígitos"), MinLength(2, ErrorMessage = "Minimo 2 dígitos")]
+        public string Nombre { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "Máximo 50 dígitos"), MinLength(2, ErrorMessage = "Minimo 2 dígitos")]
+        public string Descripcion { get; set; }
+        #endregion
+    }
+}
