fef8a71e2ee77947b68d0ef30ddf99b3e7286b2c
GreyvinMora <greyvin19@gmail.com>
2022-11-01 17:02:19 -0600

    cambio modelo cliente

--- SistemaVentasCaprichos/Shared/Cliente.cs
+++ SistemaVentasCaprichos/Shared/Cliente.cs
@@ -13,19 +13,29 @@ namespace SistemaVentasCaprichos.Shared
         [Key]
         public int Id { get; set; }
         [Required]
-        [MaxLength(300, ErrorMessage = "Máximo 300 dígitos"), MinLength(2, ErrorMessage = "Minimo 2 dígitos")]
+         [StringLength(50)]
+        [MaxLength(50, ErrorMessage = "Máximo 50 dígitos"), MinLength(2, ErrorMessage = "Minimo 2 dígitos")]
         public string NombreyApellido { get; set; }
         [Required]
+         [StringLength(16)]
         [MaxLength(16, ErrorMessage = "Máximo 16 dígitos"), MinLength(2, ErrorMessage = "Minimo 2 dígitos")]
         // buscar algún regex de cédula
         public string Cedula { get; set; }
         [Required, EnumDataType(typeof(Sexos))]
+
+        
         public Sexos Sexo { get; set; }
         [Required]
+         [StringLength(200)]
+        [MaxLength(200, ErrorMessage = "Máximo 200 dígitos"), MinLength(2, ErrorMessage = "Minimo 2 dígitos")]
         public string Dirección { get; set; }
         [Required]
+        [StringLength(20)]
+         [MaxLength(20, ErrorMessage = "Máximo 20 dígitos"), MinLength(2, ErrorMessage = "Minimo 2 dígitos")]
         [Phone(ErrorMessage = "Sólo números")]
         public string Teléfono { get; set; }
+
+        
         public decimal Saldo { get; set; }
         public DateTime FechaAlta { get; set; } = DateTime.Now;
         public bool Estado { get; set; } = true;
