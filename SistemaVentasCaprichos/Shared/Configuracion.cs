87eee196d546f26c535f823499d1d76f9ab01d9f
GreyvinMora <greyvin19@gmail.com>
2022-11-02 00:38:08 -0600

    cambios modelos configuration, cuentacorriente

--- SistemaVentasCaprichos/Shared/Configuracion.cs
+++ SistemaVentasCaprichos/Shared/Configuracion.cs
@@ -12,22 +12,29 @@ namespace SistemaVentasCaprichos.Shared
     {
         [Key]
         public int Id { get; set; }
-        [Required(ErrorMessage = "No puede dejar este campo {0} vacio")]
-        [StringLength(30, ErrorMessage = "Máximo 30 caracteres")]
+        [Required]
+         [StringLength(50)]
+        [MaxLength(50, ErrorMessage = "Máximo 50 dígitos"), MinLength(2, ErrorMessage = "Minimo 2 dígitos")]
 
         public string NombreSistema { get; set; }
-        [Required(ErrorMessage = "No puede dejar este campo {0} vacio")]
-        [StringLength(30, ErrorMessage = "Máximo 30 caracteres")]
+
+        [Required]
+        [StringLength(50)]
+        [MaxLength(50, ErrorMessage = "Máximo 50 dígitos"), MinLength(2, ErrorMessage = "Minimo 2 dígitos")]
         public string NombrePropietario { get; set; }
-        [Required(ErrorMessage = "No puede dejar este campo {0} vacio")]
-        [StringLength(100, ErrorMessage = "Máximo 100 caracteres")]
+        
+       
+        [Required]
+        [StringLength(200)]
+        [MaxLength(200, ErrorMessage = "Máximo 200 dígitos"), MinLength(2, ErrorMessage = "Minimo 2 dígitos")]
         public string Direccion { get; set; }
+
         [Required(ErrorMessage = "No puede dejar este campo {0} vacio")]
-        [StringLength(300, ErrorMessage = "Máximo 300 caracteres")]
+        [StringLength(20, ErrorMessage = "Máximo 20 caracteres"), MinLength(2, ErrorMessage = "Minimo 2 dígitos")]
         [Phone(ErrorMessage = "Este no es un número telefónico válido")]
         public string Telefono { get; set; }
         
-        [StringLength(40, ErrorMessage = "Máximo 40 caracteres")]
+        [StringLength(40, ErrorMessage = "Máximo 40 caracteres"), MinLength(2, ErrorMessage = "Minimo 2 dígitos")]
         [EmailAddress(ErrorMessage = "La proporcionada no es una dirección de correo válida")]
         public string? Email { get; set; }
         [Required(ErrorMessage = "No puede dejar este campo {0} vacio")]
